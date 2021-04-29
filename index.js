ee091c7aaf637bfce06b97801727eac6e213ed98
Megan Day <meganeday7@gmail.com>
2021-04-28 21:28:05 -0500

    commit tree 8ea5411330261897cf98591f2947965ab718b8ac

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (array) => [...array.slice(0,2)];
+
+const returnLastTwoDrivers = (array) => [...array.slice(2,4)];
+
+let selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+const fareDoubler = (fare) => fare * 2;
+const fareTripler = (fare) => fare * 3;
+const fareQuadrupler = (fare) => fare * 4;
+const fareQuintupler = (fare) => fare * 5;
+
+const createFareMultiplier = (fare) => {
+    if (fare == 2) {
+        return fareDoubler;
+    } else if (fare == 3) {
+        return fareTripler;
+    } else if (fare == 4) {
+        return fareQuadrupler;
+    } else if (fare == 5) {
+        return fareQuintupler;
+    } else {
+        return undefined}
+}
+
+const selectDifferentDrivers = (array, x) => {
+    if (x == selectingDrivers[0]) {
+        return returnFirstTwoDrivers
+    } else if (x == selectingDrivers[1]) {
+        return returnLastTwoDrivers;
+    }
+}
